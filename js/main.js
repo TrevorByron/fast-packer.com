26e482ee4f0e60cbe2a86d6f4c2bb038d0093999
Trevor <trevorborden@gmail.com>
2017-01-30 13:10:27 -0600

    heros now set up with padding and not height

--- js/main.js
+++ js/main.js
@@ -1,50 +1,10 @@
 
-//
-//Bootstrap slider
-//
-// var $item = $('.carousel .item');
-// var $wHeight = $(window).height();
-// $item.eq(0).addClass('active');
-// $item.height($wHeight);
-// $item.addClass('full-screen');
-//
-// $('.carousel img').each(function() {
-//   var $src = $(this).attr('src');
-//   var $color = $(this).attr('data-color');
-//   $(this).parent().css({
-//     'background-image' : 'url(' + $src + ')',
-//     'background-color' : $color
-//   });
-//   $(this).remove();
-// });
-//
-// $(window).on('resize', function (){
-//   $wHeight = $(window).height();
-//   $item.height($wHeight);
-// });
-//
-// $('.carousel').carousel({
-//   interval: 6000,
-//   pause: "false"
-// });
-
-
-
-
-
-
-
-
-
-
-
-
 
 // secondary nav
 
 $(function(){
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 30) {
+    $(document).scroll(function() {
+        if ($(this).scrollTop() > 1) {
             $('.second-nav').css('visibility','visible');
             $('.second-nav').fadeIn('slow');
         }
